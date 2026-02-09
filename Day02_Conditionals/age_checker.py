069badf572f8754aac2881b6530f9e91faf1ccc6
Agaba Solomon Amogo <amogoagaba@gmail.com>
2026-02-09 17:52:44 +0000

    add age checker

---
+++ Day02_Conditionals/age_checker.py
@@ -0,0 +1,12 @@
+age = int(input("Input age: "))
+
+if age < 0:
+    print("Invalid age")
+elif age < 13:
+    print("Child")
+elif age < 13:
+    print("Teen")
+elif age < 20:
+    print("Adult")
+elif age < 60:
+    print("senior")
