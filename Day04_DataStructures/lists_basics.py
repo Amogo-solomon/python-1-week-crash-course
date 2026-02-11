16629de3cb411f85d5b45905a1a6b6b91dcb15ca
Agaba Solomon Amogo <amogoagaba@gmail.com>
2026-02-11 12:10:33 +0000

    list operations in data structure

---
+++ Day04_DataStructures/lists_basics.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "banana", "orange"]
+
+print("First fruit:", fruits[0])
+
+fruits.append("mango")
+print("Updated list:", fruits)
+
+fruits.remove("banana")
+print("after removal:", fruits)
+
+#notic 
+# tuples. cannot change
+# used for fixed data
