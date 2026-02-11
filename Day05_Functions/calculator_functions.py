1f3b7c898e2fcb5f1437e3db44749f40ac93be46
Agaba Solomon Amogo <amogoagaba@gmail.com>
2026-02-11 23:59:57 +0000

    calculator functions build

---
+++ Day05_Functions/calculator_functions.py
@@ -0,0 +1,14 @@
+# return values
+
+def add(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Sum:", add(x, y))
+print("Difference:", substract(x, y))
+
